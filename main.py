22ce04a2dcc839099fc5b96762d2790f19a07c97
Scarfluix <anthony-bstr@outlook.fr>
2024-05-17 16:55:50 +0200

    Ajout du main

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 17 16:07:45 2024
+
+@author: Anthony Bastier
+"""
+
+####### Imports #######
+from mainrunapp import *
+
+####### Programme principal #######
+joueur = Joueur()
+run_app()
